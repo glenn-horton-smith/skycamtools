703384894f28206a03ae33dacfdfbd7079a67e71
Glenn Horton-Smith <gahs@phys.ksu.edu>
2018-12-31 11:18:14 -0600

    Use matplotlib to show static image to avoid losing screen focus, possibly allow future gui.

--- manual_sort.py
+++ manual_sort.py
@@ -1,9 +1,10 @@
 import sys
 import os
 import subprocess
-# import signal
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
 
-JPG_VIEW_COMMAND=['display','-immutable','-geometry','+0+0']
+#JPG_VIEW_COMMAND=['display','-immutable','-geometry','+0+0']
 MP4_VIEW_COMMAND=['vlc']
 
 def manual_sort(dirin, dirout):
@@ -18,16 +19,27 @@ def manual_sort(dirin, dirout):
                     if fn[0] == 'c' and fn.endswith('.csv') )
     viewer_log = open('manual_sort_viewer_log.txt','w')
     csvlist.sort()
+    fig = plt.figure(1)
+    figprompt = fig.text(0, 1, prompt, verticalalignment='top', wrap=True)
+    fig.canvas.set_window_title(sys.argv[0])
+    fig.show()
     for csv in csvlist:
         t = csv[1:-4]
         print(t)
         # pid = os.spawnlp(os.P_NOWAIT, VIEWER_COMMAND, VIEWER_COMMAND,
         #                  '%s/j%s.jpg'%(dirin, t))
-        proc = subprocess.Popen(JPG_VIEW_COMMAND + ['%s/j%s.jpg'%(dirin, t)],
-                                stdin=subprocess.DEVNULL,
-                                stdout=viewer_log, stderr=subprocess.STDOUT)
+        # proc = subprocess.Popen(JPG_VIEW_COMMAND + ['%s/j%s.jpg'%(dirin, t)],
+        #                         stdin=subprocess.DEVNULL,
+        #                         stdout=viewer_log, stderr=subprocess.STDOUT)
+        img = mpimg.imread('%s/j%s.jpg'%(dirin, t))
+        imgplot = plt.imshow(img)
+        ax = fig.axes[0]
+        ax.set_title(t)
+        fig.canvas.set_window_title(sys.argv[0] + " " + t)
+        fig.canvas.draw_idle()
         proc2 = None
         while True:
+            fig.canvas.draw_idle()
             answer = input(prompt)
             if answer == '?' and proc2 == None:
                 # pid2 = os.spawnlp(os.P_NOWAIT, VIEWER_COMMAND, VIEWER_COMMAND,
@@ -49,7 +61,8 @@ def manual_sort(dirin, dirout):
         # os.kill(pid, signal.SIGTERM)
         # if pid2 != None:
         #     os.kill(pid2, signal.SIGTERM)
-        proc.terminate()
+        #proc.terminate()
+        ax.clear()
         if proc2:
             proc2.terminate()
     print("All files processed.")
